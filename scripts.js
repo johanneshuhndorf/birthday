52a4fb1a576273064f87fda9dca8900d04d4ae0e
johanneshuhndorf <johanneshuhndorf@gmail.com>
2022-11-17 23:00:37 +0100

    fixes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
+// This function starts when you click the button.
+
 function unpack() {
+  // First an option window ...
+
   if (confirm("Hold on a moment!\nDo you have adblock installed?")) {
     alert("That's wonderful.");
   } else {
     alert("Oh dear .......");
   }
 
+  // Here we load the graphic.
+
   let backgroundImageURL = "url('present.svg')";
   document.getElementsByTagName("body")[0].style.backgroundImage =
     backgroundImageURL;
+
+  // Here we hide the initial text.
 
   [].forEach.call(
     document.getElementsByClassName("wrapping"),
@@ -15,6 +23,8 @@ function unpack() {
       element.style.display = "none";
     }
   );
+
+  // And here we show the hidden text.
 
   [].forEach.call(
     document.getElementsByClassName("contents"),
